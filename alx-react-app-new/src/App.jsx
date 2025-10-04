94468e8cd93ef9052e8d6918903c56cf362a2f84
Uwituze David <uwituzedavide@gmail.com>
2025-10-04 11:55:22 +0200

    Simple Counter App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -36,6 +36,8 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <h1 style={{ textAlign: 'center' }}>Simple Counter App</h1>
+      <Counter />
       <Footer />
      
     </>
