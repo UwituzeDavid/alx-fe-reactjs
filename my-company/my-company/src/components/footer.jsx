9e1cec69a7646015df8ec6b628b7e60a92a3d9eb
Uwituze David <uwituzedavide@gmail.com>
2025-10-05 22:48:22 +0200

    Build a Simple Company Website with React

---
+++ my-company/my-company/src/components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+        <p>© 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
