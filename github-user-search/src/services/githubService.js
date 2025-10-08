4366983e0b5763f35ae5b6bb49aa2b47f04cb52a
Uwituze David <uwituzedavide@gmail.com>
2025-10-08 13:17:51 +0200

    Ready for Vercel deployment

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -14,3 +14,10 @@ export const fetchAdvancedUsers = async ({ username, location, minRepos }) => {
     const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}`);
     return response.data;
   };
+  const token = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+const response = await axios.get(`https://api.github.com/users/${username}`, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
