9efd1a9c16e1da0fb007c35a0222fc26e9dab1b3
Uwituze David <uwituzedavide@gmail.com>
2025-10-08 11:17:43 +0200

    new change

--- github-user-search/src/components/SearchBar.jsx
+++ github-user-search/src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-{error && <p>Looks like we can't find the user.</p>}
+{loading && <p>Loading...</p>}
+{error && <p>Looks like we cant find the user</p>}
+{user && (
+  <div style={{ border: '1px solid #ccc', padding: '20px', maxWidth: '400px' }}>
+    <img src={user.avatar_url} alt={user.login} width="100" />
+    <h2>{user.name || user.login}</h2>
+    <p>
+      <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+        View GitHub Profile
+      </a>
+    </p>
+  </div>
+)}
