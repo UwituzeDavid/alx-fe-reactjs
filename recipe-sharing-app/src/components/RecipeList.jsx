6a7e213371bb4ad70d0a01f3387e34a7e12ca1c0
Uwituze David <uwituzedavide@gmail.com>
2025-10-15 02:52:50 +0200

    adding a link

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 import { useRecipeStore } from '../components/recipeStore';
-
+import { Link } from 'react-router-dom'; 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.filteredRecipes);
 
@@ -10,7 +10,9 @@ const RecipeList = () => {
       ) : (
         recipes.map((recipe) => (
           <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
+            <h3>
+              <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link> 
+              </h3>
             <p>{recipe.description}</p>
           </div>
         ))
