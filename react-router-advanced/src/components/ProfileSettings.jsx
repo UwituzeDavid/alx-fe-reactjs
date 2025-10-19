f5e07b659e2448f5bc5149d585a0d1f458555186
Uwituze David <uwituzedavide@gmail.com>
2025-10-19 15:03:51 +0200

    new one

--- react-router-advanced/src/components/ProfileSettings.jsx
+++ react-router-advanced/src/components/ProfileSettings.jsx
@@ -1,8 +1,3 @@
-// ProfileDetails.jsx
-export default function ProfileDetails() {
-    return <div>Profile Details Page</div>;
-  }
-  
   // ProfileSettings.jsx
   export default function ProfileSettings() {
     return <div>Profile Settings Page</div>;
--- react-router-advanced/src/components/profileDetails.jsx
+++ react-router-advanced/src/components/profileDetails.jsx
@@ -3,8 +3,3 @@ export default function ProfileDetails() {
     return <div>Profile Details Page</div>;
   }
   
-  // ProfileSettings.jsx
-  export default function ProfileSettings() {
-    return <div>Profile Settings Page</div>;
-  }
-  
