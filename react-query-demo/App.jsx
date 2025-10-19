772fe924d03ccdd214d8e9f2da4ae162625b5ac6
Uwituze David <uwituzedavide@gmail.com>
2025-10-19 13:34:26 +0200

    Adding new one

--- react-query-demo/App.jsx
+++ react-query-demo/App.jsx
@@ -1,12 +1,14 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './components/PostsComponent';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './PostsComponent'; 
 
+// Create a client
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-gray-100 p-6">
+      <div className="App">
+        <h1>React Query Setup</h1>
         <PostsComponent />
       </div>
     </QueryClientProvider>
