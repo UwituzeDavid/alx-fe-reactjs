f22ce94c407219e652812896fe34362b93b6b0a2
Uwituze David <uwituzedavide@gmail.com>
2025-10-19 14:36:15 +0200

    addition

--- react-router-advanced/src/pages/Profile.jsx
+++ react-router-advanced/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-import ProfileDetails from './ProfileDetails';
+import ProfileDetails from './ProfileDetails.jsx& ProfileSettings';
 import ProfileSettings from './ProfileSettings';
 
 function Profile() {
